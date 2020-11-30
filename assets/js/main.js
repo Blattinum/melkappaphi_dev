049fd63fed062e3ed3deab978476a8205fb4102c
Blattinum <collin.blatt@gmail.com>
2020-11-30 17:37:53 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ let url = "https://melkappaphi.cb3.xyz/";
 let newDate = new Date();
 let year = newDate.getFullYear();
 
-document.getElementsByTagName("header")[0].insertAdjacentHTML("beforeend", `<div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div>`);
+//insert header and footer
+document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", "<header></header>");
+document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", "<footer></footer>");
 
+//modify header and footer
+document.getElementsByTagName("header")[0].insertAdjacentHTML("beforeend", `<div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div>`);
 document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", `<a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a>`);
