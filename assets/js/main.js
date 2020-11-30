12b3b9b104f9b04ae22d2a4c921887e772bc788d
blattinum <collin.blatt@gmail.com>
2020-11-30 16:30:11 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 let url = "https://melkappaphi.cb3.xyz/";
 
-function clickHeaderH1() {
-    window.open(url,"_self");
-}
+let newDate = new Date();
+let year = newDate.getFullYear();
 
-document.getElementsByTagName("header")[0].insertAdjacentHTML("beforeend", `<div class="title"><h1 onclick="clickHeaderH1()">Mel Kappa Phi</h1></div>`);
+document.getElementsByTagName("header")[0].insertAdjacentHTML("beforeend", `<div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div>`);
 
-document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", `<a href="` + url + `">&copy; 2020 Mel Kappa Phi</a>`);
+document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", `<a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a>`);
