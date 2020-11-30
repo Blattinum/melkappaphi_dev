7c795ad635606d8dbf84d07d83955ba210b1e98d
blattinum <collin.blatt@gmail.com>
2020-11-30 15:07:07 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,5 @@ let url = "https://melkappaphi.cb3.xyz/";
 function clickHeaderH1() {
     window.open(url,"_self");
 }
+
+document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", `<a href="` + url + `">&copy; 2020 Mel Kappa Phi</a>`);
