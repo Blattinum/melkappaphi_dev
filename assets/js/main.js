5247992a2d1a966986f7df138acddd07a4f15c88
blattinum <collin.blatt@gmail.com>
2020-11-30 16:16:12 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,6 @@ function clickHeaderH1() {
     window.open(url,"_self");
 }
 
+document.getElementsByTagName("header")[0].insertAdjacentHTML("beforeend", `<div class="title"><h1 onclick="clickHeaderH1()">Mel Kappa Phi</h1></div>`);
+
 document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", `<a href="` + url + `">&copy; 2020 Mel Kappa Phi</a>`);
