36d76e40f364f325a4c94aab00ab9120fc054a42
blattinum <collin.blatt@gmail.com>
2020-12-01 09:19:18 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,6 @@ let url = window.location.href;
 let newDate = new Date();
 let year = newDate.getFullYear();
 
-//insert header and footer
-document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", "<header></header>");
-document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", "<footer></footer>");
-
-//modify header and footer
-document.getElementsByTagName("header")[0].insertAdjacentHTML("beforeend", `<div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div>`);
-document.getElementsByTagName("footer")[0].insertAdjacentHTML("beforeend", `<a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a>`);
+//insert and modify header and footer
+document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<header><div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div></header>`);
+document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", `<footer><a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a></footer>`);
