c852051028e2c486703dd8f9786f31342ed07a98
blattinum <collin.blatt@gmail.com>
2020-12-01 13:03:33 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,4 @@ let year = newDate.getFullYear();
 
 //insert and modify header and footer
 document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<header><div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div></header>`);
-document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", `<footer><a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a></footer>`);
+document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", `<footer><div  class="copyright"><a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a></div></footer>`);
