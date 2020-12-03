c019e30d10c646daf850a75ff7a8fbd0d7d04e45
blattinum <collin.blatt@gmail.com>
2020-12-03 17:57:08 -0500

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,13 @@ let year = newDate.getFullYear();
 document.getElementsByTagName("body")[0].insertAdjacentHTML("afterbegin", `<header><div class="title"><h1><a href="` + url + `">Mel Kappa Phi</a></h1></div></header>`);
 document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", `<footer><div  class="copyright"><a href="` + url + `">&copy; ` + year + ` Mel Kappa Phi</a></div></footer>`);
 
+let search = '^^';
+let replaceWith = '&#128522;';
 
+//const result = body.replaceAll(search, replaceWith);
+
+document.querySelector('.emoji').replaceAll(search, replaceWith);
+
+//result;
+
+//document.querySelector('.emoji').insertAdjacentHTML('beforeend',result);
